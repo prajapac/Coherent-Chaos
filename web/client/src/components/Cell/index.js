534bbcaca00eacd0c0294122ed42ac49cdad8c65
Jason Xu <jason-xu1996@hotmail.com>
2019-02-14 21:33:58 -0600

    Move cellStates from cell to constants.js
    Move cellStates from cell to constants.js
    Import constant into cell

--- web/client/src/components/Cell/index.js
+++ web/client/src/components/Cell/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import * as Constants from '../../constants/constants.js'
 
 import './index.scss';
 
@@ -8,27 +9,20 @@ const Cell = ({className, state}) => {
     const classes = classNames(className, state, 'cell');
 
     return (
-        if (state === states.player1) {
+        if (state === Constants.CELL_PLAYER_1) {
             <Hexagon className={classes} color='primary' />
         }
-        else if (state === states.player2) {
+        else if (state === Constants.CELL_PLAYER_2) {
             <Hexagon className={classes} color='secondary' />
         }
-        else if (state === states.out) {
+        else if (state === Constants.CELL_OUT_OF_PLAY) {
             <Hexagon className={classes} color='dark' />
         }
-        else if (state === states.empty) {
+        else if (state === Constants.CELL_EMPTY) {
             <Hexagon className={classes} color='light' />
         }
     );
 };
-
-enum states = {
-    Player1Pawn,
-    Player2Pawn,
-    OutOfPlay,
-    Empty,
-}
 
 Cell.propTypes = {
     className: PropTypes.string,
