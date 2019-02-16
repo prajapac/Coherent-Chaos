ab094008bbe4001facaafecdd3ee01eacece94db
Jason Xu <jason-xu1996@hotmail.com>
2019-02-16 15:45:53 -0600

    Refactor code
    Correct syntax and refactor code

--- web/client/src/components/Cell/index.js
+++ web/client/src/components/Cell/index.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import * as Constants from '../../constants/constants.js'
+import Hexagon from 'components/Hexagon';
+import { CELL_PLAYER_1, CELL_PLAYER_2, CELL_OUT_OF_PLAY, CELL_EMPTY } from 'constants/constants.js';
 
 import './index.scss';
 
 const Cell = ({className, state}) => {
     const classes = classNames(className, state, 'cell');
-    var cellColor;
+    let cellColor;
 
-    displayColor(state) {
-        switch(state)
-            case Constants.CELL_PLAYER_1:
-                cellColor = 'primary';
-                break;
-            case Constants.CELL_PLAYER_2:
-                cellColor = 'secondary';
-                break;
-            case Constants.CELL_OUT_OF_PLAY:
-                cellColor = 'dark';
-                break;
-            default:
-                cellColor = 'light';
+    switch(state) {
+        case CELL_PLAYER_1:
+            cellColor = 'primary';
+            break;
+        case CELL_PLAYER_2:
+            cellColor = 'secondary';
+            break;
+        case CELL_OUT_OF_PLAY:
+            cellColor = 'dark';
+            break;
+        default:
+            cellColor = 'light';
     }
 
     return (
@@ -31,13 +31,11 @@ const Cell = ({className, state}) => {
 
 Cell.propTypes = {
     className: PropTypes.string,
-    state: PropTypes.oneOf(Object.keys(states)),
+    state: PropTypes.oneOf([CELL_PLAYER_1, CELL_PLAYER_2, CELL_EMPTY, CELL_OUT_OF_PLAY])
 };
 
 Cell.defaultProps = {
-    state: 'empty',
+    state: CELL_EMPTY
 };
-
-Cell.states = states
 
 export default Cell;
