a8a7b522c871e4c223e96fdf381930ff82edac61
Jason Xu <jason-xu1996@hotmail.com>
2019-02-16 13:22:51 -0600

    Move switch out of return
    Change if condition to switch and move it out of return

--- web/client/src/components/Cell/index.js
+++ web/client/src/components/Cell/index.js
@@ -7,20 +7,24 @@ import './index.scss';
 
 const Cell = ({className, state}) => {
     const classes = classNames(className, state, 'cell');
+    var cellColor;
+
+    displayColor(state) {
+        switch(state)
+            case Constants.CELL_PLAYER_1:
+                cellColor = 'primary';
+                break;
+            case Constants.CELL_PLAYER_2:
+                cellColor = 'secondary';
+                break;
+            case Constants.CELL_OUT_OF_PLAY:
+                cellColor = 'dark';
+                break;
+            default:
+                cellColor = 'light';
+    }
 
     return (
-        let cellColor = 'light';
-
-        if (state === Constants.CELL_PLAYER_1) {
-            cellColor='primary';
-        }
-        else if (state === Constants.CELL_PLAYER_2) {
-            cellColor='secondary';
-        }
-        else if (state === Constants.CELL_OUT_OF_PLAY) {
-            cellColor='dark';
-        }
-
         <Hexagon className={classes} color={cellColor} />
     );
 };
