4567e046f448f0dacf137a7476ef17043f2b615c
Jason Xu <jason-xu1996@hotmail.com>
2019-03-10 21:24:23 -0500

    Change background color and constructor

--- web/client/src/components/PlayerPicker/index.js
+++ web/client/src/components/PlayerPicker/index.js
@@ -8,21 +8,22 @@ import { CELL_PLAYER_1, CELL_PLAYER_2 } from 'constants';
 
 import './index.scss';
 
-const PlayerPicker = ({className, onClick}) => {
+const PlayerPicker = ({className, onClick, onChoose}) => {
     const classes = classNames(className, 'playerPicker');
 
     return (
         <div className={classes}>
-            <Button className='btn' bold text='Player 1' type='primary' onClick={()=>{this.props.onChoose(CELL_PLAYER_1)}}/>
+            <Button className='btn' bold text='Player 1' type='primary' onClick={onChoose(CELL_PLAYER_1)}/>
             <p>Choose Your Side</p>
-            <Button className='btn' bold text='Player 2' type='secondary' onClick={()=>{this.props.onChoose(CELL_PLAYER_2)}}/>
+            <Button className='btn' bold text='Player 2' type='secondary' onClick={onChoose(CELL_PLAYER_2)}/>
         </div>
     )
 };
 
 PlayerPicker.propTypes = {
     className: PropTypes.string,
-    onClick: PropTypes.func
+    onClick: PropTypes.func,
+    onChoose: PropTypes.func
 };
 
 export default PlayerPicker;
