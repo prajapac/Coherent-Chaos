bfb79b4d2e79a02e45613c254904616c67ea1d99
CaZ-H <CaZsm@outlook.com>
2019-03-10 22:56:46 -0500

    - Cleanup

--- web/client/src/components/PlayerPicker/index.js
+++ web/client/src/components/PlayerPicker/index.js
@@ -4,26 +4,29 @@ import classNames from 'classnames';
 
 import Button from 'components/Button';
 
-import { CELL_PLAYER_1, CELL_PLAYER_2 } from 'constants';
+import { PLAYER_1, PLAYER_2 } from 'constants';
 
 import './index.scss';
 
-const PlayerPicker = ({className, onClick, onChoose}) => {
+const PlayerPicker = ({className, onChoose}) => {
     const classes = classNames(className, 'playerPicker');
 
     return (
         <div className={classes}>
-            <Button className='btn' bold text='Player 1' type='primary' onClick={()=>{onChoose(CELL_PLAYER_1)}}/>
+            <Button className='btn' bold text='Player 1' type='primary' onClick={()=>{onChoose(PLAYER_1)}}/>
             <p>Choose Your Side</p>
-            <Button className='btn' bold text='Player 2' type='secondary' onClick={()=>{onChoose(CELL_PLAYER_2)}}/>
+            <Button className='btn' bold text='Player 2' type='secondary' onClick={()=>{onChoose(PLAYER_2)}}/>
         </div>
     )
 };
 
 PlayerPicker.propTypes = {
     className: PropTypes.string,
-    onClick: PropTypes.func,
     onChoose: PropTypes.func
+};
+
+PlayerPicker.defaultProps = {
+    onChoose: () => {}
 };
 
 export default PlayerPicker;
