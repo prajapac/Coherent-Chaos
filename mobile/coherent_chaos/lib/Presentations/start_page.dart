3e20f9ef8542a604e521b02d5338cc33ecdd332e
Zack <jinhui9632@gmail.com>
2019-03-20 11:53:32 -0500

    Clean up start page Build widget to make it more readable

--- mobile/coherent_chaos/lib/Presentations/start_page.dart
+++ mobile/coherent_chaos/lib/Presentations/start_page.dart
@@ -22,7 +22,59 @@ class _StartPage extends State<StartPage> {
     final double screenHeight = MediaQuery.of(context).size.height;
     final double screenWidth = MediaQuery.of(context).size.width;
 
-    final gameDescription = Padding(
+    return Scaffold(
+      backgroundColor: colors.bodyColor,
+      appBar: AppBar(
+        title: Text(dialouges.title),
+        backgroundColor: colors.barColor,
+      ),
+      body: Center(
+        child: Container(
+          width: screenWidth * 0.8,
+          height: screenHeight * 0.6,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(4)),
+              color: colors.createJoinGameBgColor),
+          child: ListView(
+            shrinkWrap: true,
+            padding: EdgeInsets.only(left: 24.0, right: 24.0),
+            children: <Widget>[
+              getGameTitle(),
+              getGameDescription(),
+              getGameIdBox(),
+              SizedBox(
+                height: 20.0,
+              ),
+              getJoinGameButton(),
+              SizedBox(
+                height: 20.0,
+              ),
+              getCreateGameButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget getGameTitle() {
+    return Padding(
+      padding: EdgeInsets.only(top: 40.0, bottom: 20.0),
+      child: Container(
+        child: Text(
+          dialouges.startGame,
+          style: TextStyle(
+              color: Colors.grey[700],
+              fontSize: 26,
+              fontFamily: 'Montserrat',
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+
+  Widget getGameDescription() {
+    return Padding(
       padding: EdgeInsets.only(top: 10.0, bottom: 30.0),
       child: Container(
         child: Text(
@@ -34,8 +86,10 @@ class _StartPage extends State<StartPage> {
         ),
       ),
     );
+  }
 
-    final gameIdBox = TextFormField(
+  Widget getGameIdBox() {
+    return TextFormField(
       decoration: InputDecoration(
         hintText: 'A4B6',
         filled: true,
@@ -46,8 +100,10 @@ class _StartPage extends State<StartPage> {
         ),
       ),
     );
+  }
 
-    final createGame = MaterialButton(
+  Widget getCreateGameButton() {
+    return MaterialButton(
       onPressed: () async {
         final Game game = await handleAPIs.intializeGame();
         Navigator.push(
@@ -57,7 +113,7 @@ class _StartPage extends State<StartPage> {
           ),
         );
       },
-      color: colors.secondaryColor,
+      color: colors.tertiaryColor,
       height: 60.0,
       child: Text(
         dialouges.createGame,
@@ -67,8 +123,10 @@ class _StartPage extends State<StartPage> {
             fontWeight: FontWeight.bold),
       ),
     );
+  }
 
-    final joinGame = MaterialButton(
+  Widget getJoinGameButton() {
+    return MaterialButton(
       onPressed: () async {
         final Game game =
             await handleAPIs.intializeGame(); //TODO: make request to Join game
@@ -87,52 +145,6 @@ class _StartPage extends State<StartPage> {
             color: Colors.grey[200].withOpacity(0.9),
             fontSize: 20.0,
             fontWeight: FontWeight.bold),
-      ),
-    );
-
-    return Scaffold(
-      backgroundColor: colors.bodyColor,
-      appBar: AppBar(
-        title: Text(dialouges.title),
-        backgroundColor: colors.barColor,
-      ),
-      body: Center(
-        child: Container(
-          width: screenWidth * 0.8,
-          height: screenHeight * 0.6,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(4)),
-              color: colors.createJoinGameBgColor),
-          child: ListView(
-            shrinkWrap: true,
-            padding: EdgeInsets.only(left: 24.0, right: 24.0),
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(top: 40.0, bottom: 20.0),
-                child: Container(
-                  child: Text(
-                    dialouges.startGame,
-                    style: TextStyle(
-                        color: Colors.grey[700],
-                        fontSize: 26,
-                        fontFamily: 'Montserrat',
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-              gameDescription,
-              gameIdBox,
-              SizedBox(
-                height: 20.0,
-              ),
-              joinGame,
-              SizedBox(
-                height: 20.0,
-              ),
-              createGame,
-            ],
-          ),
-        ),
       ),
     );
   }
